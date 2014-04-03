6ffec7632df09506cbcfbf2678f8291fa04ce698
Darren Britton <darrenbritton@hotmail.com>
2014-04-03 10:17:18 +0100

    added AInames.txt back in

---
+++ Server/AI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class AI : Player
+    {
+        public List<string> hand;
+
+        public AI()
+        {
+            hand = new List<string>();
+
+        }
+
+        public void SeperateHand(string handString)
+        {
+            hand.AddRange(handString.Split('`').ToList());
+        }
+
+    }
+}
