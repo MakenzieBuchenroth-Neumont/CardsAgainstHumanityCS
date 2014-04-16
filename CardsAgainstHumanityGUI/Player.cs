511aa0837fffe5665ace389563d87df61bbe0df1
Darren Britton <darrenbritton@hotmail.com>
2014-04-16 20:22:12 +0100

    Added CAHgui to collection

---
+++ CardsAgainstHumanityGUI/Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsAgainstHumanity
+{
+    class Player
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private IPAddress ipAddress;
+
+        public string IpAddress
+        {
+            get { return ipAddress.ToString(); }
+            set { ipAddress = IPAddress.Parse(value); }
+        }
+        private bool isCzar;
+
+        public bool IsCzar
+        {
+            get { return isCzar; }
+            set { isCzar = value; }
+        }
+
+        private string lastDump;
+
+        public string LastDump
+        {
+            get { return lastDump; }
+            set { lastDump = value; }
+        }
+
+        public List<string> hand;
+
+
+        public Player()
+        {
+            name = "";
+            ipAddress = FindIP();
+            isCzar = false;
+            lastDump = "100:100";
+            hand = new List<string>();
+        }
+
+        public void SeperateHand(string handString)
+        {
+            hand.AddRange(handString.Split('`').ToList());
+        }
+
+        public void DisplayHand()
+        {
+            for (int i = 0; i < hand.Count; i++)
+            {
+                Console.WriteLine(i + ".\t" + hand[i] + "\n");
+            }
+        }
+
+        public IPAddress FindIP()
+        {
+            UdpClient u = new UdpClient("8.8.8.8", 1);
+            IPAddress localAddr = ((IPEndPoint)u.Client.LocalEndPoint).Address;
+            return localAddr;
+        }
+
+        public override string ToString()
+        {
+            return name + "`" + ipAddress + "`" + isCzar.ToString() + "`" + lastDump;
+        }
+    }
+}
---
+++ CardsAgainstHumanityGUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CardsAgainstHumanityGUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CardsAgainstHumanityGUI")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f8c49010-cc94-4b38-95ab-1a87af6418f3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
